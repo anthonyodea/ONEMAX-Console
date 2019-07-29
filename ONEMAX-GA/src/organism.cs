5e882896dffe19ff5f0f4384af7c82f94e7ec5af
AntCode64 <anthony@odea.net.au>
2019-07-29 15:18:46 +0930

    Added Population.cs and Organism.cs files

---
+++ ONEMAX-GA/src/organism.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONEMAX_GA.src
+{
+    class Organism
+    {
+        //Creates a list of genes with getter and setter functions
+        public List<int> genes { get; set; } = new List<int>();
+
+        //Organism constructor - takes a random generator passed from population constructor, and uses it to generate
+        //a random digit (0 or 1), then adds it to the 'genes' List. 
+        public Organism(Random rndGen, int genomeLength)
+        {
+            for (int i = 0; i < genomeLength; i++)
+            {
+                int randnum = rndGen.Next(2);
+                genes.Add(randnum);
+            }
+        }
+    }
+}
---
+++ ONEMAX-GA/src/population.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONEMAX_GA.src
+{
+    class Population
+    {
+        int popSize;
+        List<Organism> orgs = new List<Organism>();
+
+        public Population(int popSize, int genomeLength)
+        {
+            //Sets local variable 'this.popSize' to the value of parameter 'popSize' 
+            this.popSize = popSize;
+
+            //Creates a random generator 'rndGen'
+            Random rndGen = new Random();
+
+            for (int i = 0; i < popSize; i++)
+            {
+                //Creates new organism and adds it to the 'orgs' list
+                Organism newOrg = new Organism(rndGen, genomeLength);
+                orgs.Add(newOrg);
+            }
+        }
+
+        //Prints the genes of every organism in the population
+        public void print(bool checkForDuplicates = false)
+        {
+            for (int i = 0; i < this.popSize; i++)
+            {
+                for (int j = 0; j < this.orgs[i].genes.Count; j++)
+                {
+                    Console.Write(this.orgs[i].genes[j]);
+                }
+                Console.Write("     " + i + "\n");
+
+                //Prints a statement if one organism has the exact same genome as another organism
+                if (checkForDuplicates)
+                {
+                    for (int k = 0; k < i; k++)
+                    {
+                        //Method retrieved from https://stackoverflow.com/questions/22173762/check-if-two-lists-are-equal
+                        if (this.orgs[i].genes.SequenceEqual(this.orgs[k].genes) && i != k)
+                        {
+                            Console.WriteLine("Organism " + i + " == Organism " + k);
+                            //Optional program pause
+                            //Console.ReadKey();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

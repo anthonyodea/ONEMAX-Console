4a00aec1c4e3a820d77bb90d694f259768c8efab
AntCode64 <anthony@odea.net.au>
2019-07-29 09:51:20 +0930

    initial version commit

---
+++ ONEMAX-GA/src/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONEMAX_GA
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!!!");
+            Console.ReadKey();
+        }
+    }
+}

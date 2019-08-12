9d3ca72fc407754a303d94f5134d28de9c4e7d5c
AntCode64 <anthony@odea.net.au>
2019-08-12 18:46:38 +0930

    Added RandomUtils file

---
+++ ONEMAX-GA/src/RandomUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntCode64.ONEMAX_Console
+{
+    /// <summary>
+    /// Sole purpose is to provide global Random generator, so all other classes may use the same rndGen
+    /// </summary>
+    static class RandomUtils
+    {
+        public static Random RndGen { get; set; } = new Random();
+    }
+}
